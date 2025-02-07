8512049e5f7b624f13a66ff212a78392d6eb4fbc
cjafet <cjafet07@gmail.com>
2025-02-07 15:10:21 -0300

    Fix model mapper

--- src/main/java/com/message/aws/model/entity/VideoEntity.java
+++ src/main/java/com/message/aws/model/entity/VideoEntity.java
@@ -2,15 +2,19 @@ package com.message.aws.model.entity;
 
 import com.message.aws.model.enums.VideoStatus;
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "video")
 public class VideoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "video_generator")
+    @SequenceGenerator(name = "video_generator", sequenceName = "video_id_sequence", allocationSize = 1)
     private Long id;
 
     @Column(name = "status")
+    @Enumerated(EnumType.STRING)
     private VideoStatus videoStatus;
 
     @Column(name = "url")

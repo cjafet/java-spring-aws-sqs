40d8071cd4c0005731926cb1e185a83466078e98
cjafet <cjafet07@gmail.com>
2025-02-07 23:38:14 -0300

    Fix token validation

--- src/main/java/com/message/aws/service/JwtService.java
+++ src/main/java/com/message/aws/service/JwtService.java
@@ -2,6 +2,7 @@ package com.message.aws.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.message.aws.model.entity.User;
 import com.message.aws.security.TokenPayload;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -22,9 +23,12 @@ public class JwtService {
         return payload.getName();
     }
 
-    public boolean isTokenValid(String token, UserDetails userDetails) {
+    public boolean isTokenValid(String token, UserDetails userDetails, String requestURI) {
         final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()));
+        User user = (User) userDetails;
+        String[] requestPath = requestURI.split("/");
+        Long userId = Long.parseLong(requestPath[requestPath.length-1]);
+        return (username.equals(userDetails.getUsername()) && userId.equals(user.getId()));
     }
 
 

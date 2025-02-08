567e42f6208c2f74ddc527e590fa1666f99cca9d
Carlos Jafet Neto <cjafet07@gmail.com>
2025-02-07 23:58:38 -0300

    Refactor isTokenValid method

--- src/main/java/com/message/aws/application/service/JwtService.java
+++ src/main/java/com/message/aws/application/service/JwtService.java
@@ -1,8 +1,9 @@
-package com.message.aws.application.service;
+package com.message.aws.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.message.aws.infrastructure.security.TokenPayload;
+import com.message.aws.model.entity.User;
+import com.message.aws.security.TokenPayload;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +14,28 @@ public class JwtService {
 
     public String extractUsername(String token) {
         Gson gson = new GsonBuilder().create();
-
-        String[] chunks = token.split("\\.");
-        Base64.Decoder decoder = Base64.getUrlDecoder();
-
-        TokenPayload payload = gson.fromJson(new String(decoder.decode(chunks[1])), TokenPayload.class);
+        TokenPayload payload = gson.fromJson(extractPayload(token), TokenPayload.class);
 
         return payload.getName();
     }
 
-    public boolean isTokenValid(String token, UserDetails userDetails) {
+    public boolean isTokenValid(String token, UserDetails userDetails, String requestURI) {
         final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()));
+        final Long userId = extractUserId(userDetails, requestURI);
+
+        return (username.equals(userDetails.getUsername()) && userId.equals(userId));
+    }
+
+    private String extractPayload(String token) {
+        String[] chunks = token.split("\\.");
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        return new String(decoder.decode(chunks[1]));
+    }
+
+    private Long extractUserId(UserDetails userDetails, String requestURI) {
+        User user = (User) userDetails;
+        String[] requestPath = requestURI.split("/");
+        return Long.parseLong(requestPath[requestPath.length-1]);
     }
 
 
